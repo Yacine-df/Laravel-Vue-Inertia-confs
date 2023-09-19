e21df94a6e90643ed2c72a9a6c27bdec60745180
Yacine-df <ycn.diaf@gmail.com>
2023-09-19 17:24:44 +0200

    Laravel-vue-inertia-tailwind confs

---
+++ resources/js/Pages/main.vue
@@ -0,0 +1,12 @@
+<script setup>
+
+</script>
+<template>
+    <div class="grid place-items-center p-4 font-semibold">
+        Welcome, This is Laravel using inertia and Vue !
+    </div>
+</template>
+
+<style>
+
+</style>
